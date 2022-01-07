484698ac90db89c628269bfaf8fe8a1285b853ff
leeing <372711472@qq.com>
2022-01-07 17:17:42 +0800

    remove key

--- webdemo/im/js/config.js
+++ webdemo/im/js/config.js
@@ -3,25 +3,25 @@
   var envir = 'online';
   var configMap = {
     dev: {
-      appkey: 'fe416640c8e8a72734219e1847ad2547',
+      appkey: '',
       url: 'https://apptest.netease.im',
       chatroomList: 'https://apptest.netease.im/api/chatroom/homeList',
       chatroomAddr: 'https://apptest.netease.im/api/chatroom/requestAddress'
     },
     test: {
-      appkey: 'fe416640c8e8a72734219e1847ad2547',
+      appkey: '',
       url: 'https://apptest.netease.im',
       chatroomList: 'https://apptest.netease.im/api/chatroom/homeList',
       chatroomAddr: 'https://apptest.netease.im/api/chatroom/requestAddress'
     },
     pre: {
-      appkey: '45c6af3c98409b18a84451215d0bdd6e',
+      appkey: '',
       url: 'https://apptest.netease.im',
       chatroomList: 'https://apptest.netease.im/api/chatroom/homeList',
       chatroomAddr: 'https://apptest.netease.im/api/chatroom/requestAddress'
     },
     online: {
-      appkey: '45c6af3c98409b18a84451215d0bdd6e',
+      appkey: '',
       url: 'https://app.netease.im',
       chatroomList: 'https://app.netease.im/api/chatroom/homeList',
       chatroomAddr: 'https://app.netease.im/api/chatroom/requestAddress'
